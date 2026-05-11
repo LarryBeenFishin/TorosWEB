94009d391d3065ad43daba4bfd575385f5735678
LarryBeenFishin <arthur.sanchez2024@gmail.com>
2026-05-11 01:40:10 -0600

    Create get-customers.js

---
+++ api/get-customers.js
@@ -0,0 +1,18 @@
+module.exports = async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `${process.env.APPOINTMENTS_SCRIPT_URL}?action=getCustomers&password=${process.env.APPS_SCRIPT_ADMIN_PASSWORD}`
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({
+      status: "error",
+      message: err.message,
+      customers: []
+    });
+  }
+};
