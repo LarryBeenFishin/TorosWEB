4adfd1bc7fc9ba997fec6ea84eeac93728926f5a
LarryBeenFishin <arthur.sanchez2024@gmail.com>
2026-05-10 22:32:54 -0600

    Create send-sms.js

---
+++ api/send-sms.js
@@ -0,0 +1,41 @@
+const twilio = require("twilio");
+
+module.exports = async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      error: "Method not allowed"
+    });
+  }
+
+  try {
+
+    const { to, message } = req.body;
+
+    const client = twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    );
+
+    const result = await client.messages.create({
+      body: message,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: to
+    });
+
+    return res.status(200).json({
+      success: true,
+      sid: result.sid
+    });
+
+  } catch (err) {
+
+    return res.status(500).json({
+      success: false,
+      error: err.message
+    });
+
+  }
+
+};
